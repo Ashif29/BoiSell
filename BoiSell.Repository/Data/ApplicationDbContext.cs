76e9ba968d3ad2a33ba3e9567fdc9b87a1113930
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-28 13:14:14 +0600

    Saving enum  as string

--- BoiSell.Repository/Data/ApplicationDbContext.cs
+++ BoiSell.Repository/Data/ApplicationDbContext.cs
@@ -14,7 +14,7 @@ namespace BoiSell.Web.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(builder); 
+            base.OnModelCreating(builder);
 
             builder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Self Help" },
@@ -34,85 +34,92 @@ namespace BoiSell.Web.Data
                 new Category { Id = 15, Name = "Travel" }
             );
 
-            // Seed books
-            builder.Entity<Book>().HasData(
-                new Book
-                {
-                    Id = 1,
-                    Title = "Atomic Habits",
-                    Author = "James Clear",
-                    ISBN = Guid.NewGuid().ToString(),
-                    Description = "Tiny Changes, Remarkable Results.No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results.",
-                    Price = 1000,
-                    Discount = 5,
-                    SpecialTag = "Classic",
-                    Publisher = "Scribner",
-                    PublishedDate = new DateOnly(1925, 4, 10),
-                    Pages = 180,
-                    Language = "English",
-                    CategoryId = 1,
-                    ImageUrl = "/images/books/AtomicHabits.jpg",
-                    Status = BookStatus.Available,
-                    CreatedAt = DateTime.Now
-                },
-                new Book
-                {
-                    Id = 2,
-                    Title = "Sapiens: A Brief History of Humankind",
-                    Author = "Yuval Noah Harari",
-                    ISBN = Guid.NewGuid().ToString(),
-                    Description = "A look at the history and impact of Homo sapiens.The book, focusing on Homo sapiens, surveys the history of humankind, starting from the Stone Age and going up to the twenty-first century.",
-                    Price = 300,
-                    Discount = 0,
-                    SpecialTag = "Bestseller",
-                    Publisher = "Harvill Secker",
-                    PublishedDate = new DateOnly(2011, 9, 4),
-                    Pages = 443,
-                    Language = "English",
-                    CategoryId = 2,
-                    ImageUrl = "/images/books/BlueChristanity.jpg",
-                    Status = BookStatus.Available,
-                    CreatedAt = DateTime.Now
-                },
-                new Book
-                {
-                    Id = 3,
-                    Title = "A Brief History of Time",
-                    Author = "Stephen Hawking",
-                    ISBN = Guid.NewGuid().ToString(),
-                    Description = "A landmark book in cosmology.A simple summary of A Brief History of Time goes all the way from the beginning of the universe to its end, explaining things like space and time, the expanding universe, the uncertainty principle, black holes, wormholes, and time travel along the way. It sold over 25 million copies.",
-                    Price = 120,
-                    Discount = 10,
-                    SpecialTag = "Science",
-                    Publisher = "Bantam Books",
-                    PublishedDate = new DateOnly(1988, 3, 1),
-                    Pages = 212,
-                    Language = "English",
-                    CategoryId = 3,
-                    ImageUrl = "/images/books/Celopatra.jpg",
-                    Status = BookStatus.Available,
-                    CreatedAt = DateTime.Now
-                },
-                new Book
-                {
-                    Id = 4,
-                    Title = "Twilight",
-                    Author = "Stephenie Meyer",
-                    ISBN = Guid.NewGuid().ToString(),
-                    Description = "A vampire romance novel.Twilight is a captivating novel by Stephenie Meyer. Set in the small town of Forks, it tells the story of Bella, a teenager who falls in love with a vampire named Edward. Their unconventional relationship unfolds in a world where love and danger collide.",
-                    Price = 900,
-                    Discount = 0,
-                    SpecialTag = "Popular",
-                    Publisher = "Little, Brown and Company",
-                    PublishedDate = new DateOnly(2005, 10, 5),
-                    Pages = 498,
-                    Language = "English",
-                    CategoryId = 4,
-                    ImageUrl = "/images/books/twilight.jpg",
-                    Status = BookStatus.Available,
-                    CreatedAt = DateTime.Now
-                }
-            );
+
+
+            builder.Entity<Book>(entity =>
+            {
+                entity.Property(e => e.Status).HasConversion<string>();
+
+                // Seed books
+                entity.HasData(
+                    new Book
+                    {
+                        Id = 1,
+                        Title = "Atomic Habits",
+                        Author = "James Clear",
+                        ISBN = Guid.NewGuid().ToString(),
+                        Description = "Tiny Changes, Remarkable Results.No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results.",
+                        Price = 1000,
+                        Discount = 5,
+                        SpecialTag = "Classic",
+                        Publisher = "Scribner",
+                        PublishedDate = new DateOnly(1925, 4, 10),
+                        Pages = 180,
+                        Language = "English",
+                        CategoryId = 1,
+                        ImageUrl = "/images/books/AtomicHabits.jpg",
+                        Status = BookStatus.Available,
+                        CreatedAt = DateTime.Now
+                    },
+                    new Book
+                    {
+                        Id = 2,
+                        Title = "Sapiens: A Brief History of Humankind",
+                        Author = "Yuval Noah Harari",
+                        ISBN = Guid.NewGuid().ToString(),
+                        Description = "A look at the history and impact of Homo sapiens.The book, focusing on Homo sapiens, surveys the history of humankind, starting from the Stone Age and going up to the twenty-first century.",
+                        Price = 300,
+                        Discount = 0,
+                        SpecialTag = "Bestseller",
+                        Publisher = "Harvill Secker",
+                        PublishedDate = new DateOnly(2011, 9, 4),
+                        Pages = 443,
+                        Language = "English",
+                        CategoryId = 2,
+                        ImageUrl = "/images/books/BlueChristanity.jpg",
+                        Status = BookStatus.Available,
+                        CreatedAt = DateTime.Now
+                    },
+                    new Book
+                    {
+                        Id = 3,
+                        Title = "A Brief History of Time",
+                        Author = "Stephen Hawking",
+                        ISBN = Guid.NewGuid().ToString(),
+                        Description = "A landmark book in cosmology.A simple summary of A Brief History of Time goes all the way from the beginning of the universe to its end, explaining things like space and time, the expanding universe, the uncertainty principle, black holes, wormholes, and time travel along the way. It sold over 25 million copies.",
+                        Price = 120,
+                        Discount = 10,
+                        SpecialTag = "Science",
+                        Publisher = "Bantam Books",
+                        PublishedDate = new DateOnly(1988, 3, 1),
+                        Pages = 212,
+                        Language = "English",
+                        CategoryId = 3,
+                        ImageUrl = "/images/books/Celopatra.jpg",
+                        Status = BookStatus.Available,
+                        CreatedAt = DateTime.Now
+                    },
+                    new Book
+                    {
+                        Id = 4,
+                        Title = "Twilight",
+                        Author = "Stephenie Meyer",
+                        ISBN = Guid.NewGuid().ToString(),
+                        Description = "A vampire romance novel.Twilight is a captivating novel by Stephenie Meyer. Set in the small town of Forks, it tells the story of Bella, a teenager who falls in love with a vampire named Edward. Their unconventional relationship unfolds in a world where love and danger collide.",
+                        Price = 900,
+                        Discount = 0,
+                        SpecialTag = "Popular",
+                        Publisher = "Little, Brown and Company",
+                        PublishedDate = new DateOnly(2005, 10, 5),
+                        Pages = 498,
+                        Language = "English",
+                        CategoryId = 4,
+                        ImageUrl = "/images/books/twilight.jpg",
+                        Status = BookStatus.Available,
+                        CreatedAt = DateTime.Now
+                    }
+                );
+            });
         }
     }
 }
---
+++ BoiSell.Repository/Data/Migrations/20240828070204_SavingEnumAsString.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoiSell.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class SavingEnumAsString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "ISBN", "Status" },
+                values: new object[] { new DateTime(2024, 8, 28, 13, 2, 3, 490, DateTimeKind.Local).AddTicks(2522), "39f78952-762a-42aa-8700-18a026a72c3f", "Available" });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "ISBN", "Status" },
+                values: new object[] { new DateTime(2024, 8, 28, 13, 2, 3, 490, DateTimeKind.Local).AddTicks(2555), "6a2535e8-1061-40d2-adfc-fd8e6c9ad745", "Available" });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "ISBN", "Status" },
+                values: new object[] { new DateTime(2024, 8, 28, 13, 2, 3, 490, DateTimeKind.Local).AddTicks(2563), "26cd2bfa-1833-4ee0-8459-68a8f483aa96", "Available" });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedAt", "ISBN", "Status" },
+                values: new object[] { new DateTime(2024, 8, 28, 13, 2, 3, 490, DateTimeKind.Local).AddTicks(2570), "5a85685f-590f-4c73-8db0-3f2d59fe8630", "Available" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "ISBN", "Status" },
+                values: new object[] { new DateTime(2024, 8, 28, 11, 48, 10, 685, DateTimeKind.Local).AddTicks(7213), "1f30e16c-a1e7-433c-af5d-1a665c1eb2a5", 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "ISBN", "Status" },
+                values: new object[] { new DateTime(2024, 8, 28, 11, 48, 10, 685, DateTimeKind.Local).AddTicks(7232), "174cfa9e-e90c-4a33-adc0-fad756a7819d", 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "ISBN", "Status" },
+                values: new object[] { new DateTime(2024, 8, 28, 11, 48, 10, 685, DateTimeKind.Local).AddTicks(7238), "cf3693a2-2252-425a-a33d-7d15ab8c104e", 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedAt", "ISBN", "Status" },
+                values: new object[] { new DateTime(2024, 8, 28, 11, 48, 10, 685, DateTimeKind.Local).AddTicks(7244), "f80ca38e-65a2-4155-aab0-aa64216d645c", 0 });
+        }
+    }
+}
